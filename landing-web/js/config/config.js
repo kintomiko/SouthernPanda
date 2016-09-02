911357557476f4c26ddd0814a75005abf2adfa76
Kin Lin <kin.lin@sparkventures.co.nz>
2016-09-03 11:46:03 +1200

    change repo structure

--- landing-web/js/config/config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by kinlin on 8/27/16.
- */
-
-
-app.constant('SERVER_URL', 'http://127.0.0.1:8080');
-app.constant('LOGIN_API', '/user/search/findUserByEmail?email=');
-app.constant('ITEM_API', '/item');
